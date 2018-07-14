162f296943ee78ba5262f17d9a9278c131f2dc78
Grace Yoo <graceyoo33@gmail.com>
2018-07-14 05:51:08 -0400

    initial index.html, initial style.css for index file, JavaScript pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+
+
+
+// at the start of every game, there's a randomly generated number, which has a value between 19 - 120
+
+// there are 4 crystals and each have a different randomly generated number, which are hidden from the user, and values should be between 1 - 12
+
+// each time you click on a crystal, user will add the amount of points the crystal is worth to the total score
+
+// only way to find out how much a crystal is worth is to click on it
+
+// goal is to match user's total score to the random number
+
+// if user's total score is less than random number, then user keeps playing
+
+// if user's total score matches random number, then user is alerted they won (in wins/loss counter div) and counter for wins will increment by 1, and random number resets automatically, and total score is reset to 0, crystal values also resets automatically to a new random number
+
+// if user's total score is greater than random number, then user is alerted they lost in wins/loss counter div) and counter for losses will increment by 1, and random number resets automatically, and total score is reset to 0, crystal values also resets automatically to a new random number
